7ee1fce640b496c011f0210d6535956cbceb6aa2
bananaizg0d <someonefarted99@gmail.com>
2022-06-10 22:24:02 +0300

    Update Quest1Fin.cs

--- Assets/Quest1Fin.cs
+++ Assets/Quest1Fin.cs
@@ -6,12 +6,14 @@ public class Quest1Fin : MonoBehaviour
     
 {
     // Start is called before the first frame update
-    [SerializeField]GameObject Player;
+    [SerializeField] GameObject Player;
     [SerializeField] GameObject TeleDes;
+    [SerializeField] GameObject RB1;
     void Start()
     {
         Player = GameObject.Find("PlayerObject");
         TeleDes = GameObject.Find("Quest1NPCDesination");
+        RB1 = GameObject.Find("RoadBlock1");
     }
 
     // Update is called once per frame
@@ -23,6 +25,6 @@ public class Quest1Fin : MonoBehaviour
     public void Quest1End()
     {
         Player.transform.position = TeleDes.transform.position;
-
+        Destroy(RB1);
     }
 }
