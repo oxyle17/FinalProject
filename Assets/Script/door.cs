ccb19848ec04e96b437ce54cd8bcc902cf918c8e
bananaizg0d <someonefarted99@gmail.com>
2022-06-19 20:11:57 +0300

    düzgün kapı yaptım

--- Assets/Script/door.cs
+++ Assets/Script/door.cs
@@ -5,120 +5,138 @@ using UnityEngine;
 public class door : MonoBehaviour
 {
 
-    Vector3 openPos;
-    Vector3 closePos;
+    //    public int openCount;
 
-    bool isOpen = false;
-
-    public int openCount;
-
+    public Vector3 openPos;
+    public Vector3 closePos;
+    public bool isOpening = false;
+    public bool isClosing = false;
+    public float doorSpeed;
 
     void Start()
     {
-        Debug.Log("Door");
-
-        closePos = transform.position;
-
-        openPos = transform.position;
-        openPos.z += 16;
-
-
+        //Debug.Log("Door");
 
 
     }
-
-    public void Open()
+    private void Update()
     {
-
-
-        isOpen = true;
-
-
-
-
-    }
-
-
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (isOpening)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, openPos, Time.deltaTime * doorSpeed);
+        }
+        else if (isClosing)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, closePos, Time.deltaTime * doorSpeed);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-
-        openCount++;
-
-
-        if (openCount == 1)
-        {
-          if (isOpen == false)
-           {
-
-            StartCoroutine(aaa());
-
-
-
-            }
-        }
-        if (openCount > 1)
-        {
-
-        }
-        
-       
-
+        isOpening = true;
+        isClosing = false;
     }
-
 
     private void OnTriggerExit(Collider other)
     {
-        openCount++;
-       
-            StartCoroutine(bbb());
-       
-
+        isClosing = true;
+        isOpening = false;
     }
 
+    
 
-    public IEnumerator aaa()
-    {
-
-        for (; ; )
-        {
-            yield return new WaitForSeconds(0.01f);
-            transform.position = Vector3.MoveTowards(transform.position, openPos, 20 * Time.deltaTime);
-            if (isOpen == true)
-            {
-                break;
-
-            }
-
-        }
-        
-        
-    }
+    //    public void Open()
+    //    {
 
 
-    public IEnumerator bbb()
-    {
+    //        isOpen = true;
 
-        for (; ; )
-        {
 
-            yield return new WaitForSeconds(0.1f);
-            transform.position = Vector3.MoveTowards(transform.position, closePos, 70 * Time.deltaTime);
-            if (isOpen == true)
-            {
-                break;
 
-            }
 
-        }
-    }
+    //    }
+
+
+
+
+    //    // Update is called once per frame
+    //    void Update()
+    //    {
+
+    //    }
+
+    //    private void OnTriggerEnter(Collider other)
+    //    {
+
+    //        openCount++;
+
+
+    //        if (openCount == 1)
+    //        {
+    //          if (isOpen == false)
+    //           {
+
+    //            StartCoroutine(aaa());
+
+
+
+    //            }
+    //        }
+    //        if (openCount > 1)
+    //        {
+
+    //        }
+
+
+
+    //    }
+
+
+    //    private void OnTriggerExit(Collider other)
+    //    {
+    //        openCount++;
+
+    //            StartCoroutine(bbb());
+
+
+    //    }
+
+
+    //    public IEnumerator aaa()
+    //    {
+
+    //        for (; ; )
+    //        {
+    //            yield return new WaitForSeconds(0.01f);
+    //            transform.position = Vector3.MoveTowards(transform.position, openPos, 20 * Time.deltaTime);
+    //            if (isOpen == true)
+    //            {
+    //                break;
+
+    //            }
+
+    //        }
+
+
+    //    }
+
+
+    //    public IEnumerator bbb()
+    //    {
+
+    //        for (; ; )
+    //        {
+
+    //            yield return new WaitForSeconds(0.1f);
+    //            transform.position = Vector3.MoveTowards(transform.position, closePos, 70 * Time.deltaTime);
+    //            if (isOpen == true)
+    //            {
+    //                break;
+
+    //            }
+
+    //        }
+    //    }
 
 
 
